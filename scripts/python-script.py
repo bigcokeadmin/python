9be916009c54616facef4bf97cc467f5bb8e0c91
niranjan.hettiwatte@virtustream.com <niranjan.hettiwatte@serendib-us.com>
2023-09-14 21:26:09 -0700

    Troubleshoot script

--- scripts/python-script.py
+++ scripts/python-script.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import os
 import subprocess
-result = subprocess.run(["ls", os.environ['FILENAME'])
+result = subprocess.run(["ls", "-l"] )
+#result = subprocess.run(["ls", os.environ['FILENAME'])
 result
 print( "Hello USERNAME")
 
